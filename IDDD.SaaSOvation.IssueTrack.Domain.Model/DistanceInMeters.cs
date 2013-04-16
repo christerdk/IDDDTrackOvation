ce2a39047669744f2e58a8e86f81d238796d44b0
Christer Ostergaard <christer@christer.dk>
2013-04-16 17:01:46 +0200

    Meters prototype

---
+++ IDDD.SaaSOvation.IssueTrack.Domain.Model/DistanceInMeters.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDDD.SaaSOvation.IssueTrack.Domain.Model
+{
+    public class DistanceInMeters
+    {
+        public int Amount { get; protected set; }
+
+        public DistanceInMeters(int amount)
+        {
+            Amount = amount;
+        }
+    }
+}
