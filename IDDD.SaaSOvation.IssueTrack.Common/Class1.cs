138cc4d02b255e7e92daee5250de3357ae03835a
Christer Ostergaard <christer@christer.dk>
2013-04-17 14:30:33 +0200

    Added Common and ProductDefectStatistics

---
+++ IDDD.SaaSOvation.IssueTrack.Common/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDDD.SaaSOvation.IssueTrack.Common
+{
+    public class EventPublisher
+    {
+        private EventPublisher()
+        {
+            
+        }
+        private static EventPublisher s_eventPublisher;
+
+        public static EventPublisher Instance { get
+        {
+            if (s_eventPublisher == null)
+                s_eventPublisher = new EventPublisher();
+            return s_eventPublisher;
+        }}
+
+        public void Publish(object @event)
+        {
+            
+        }
+    }
+}
---
+++ IDDD.SaaSOvation.IssueTrack.Domain.Model/Services/ProductDefectStatistics.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IDDD.SaaSOvation.IssueTrack.Domain.Model.Services
+{
+    public class ProductDefectStatistics
+    {
+         public IList<Product> AllProductsOfTenant(TenantId tenantId)
+         {
+             return null;
+         }
+
+        public ProductId MostDefectiveProduct()
+        {
+            return new ProductId(Guid.Empty);
+        }
+
+    }
+}
